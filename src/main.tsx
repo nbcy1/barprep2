3c035309aec18927ad007381f12255422d1bc17c
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-09 18:23:49 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,9 @@ import './index.css'
 
 // Amplify setup
 import { Amplify } from 'aws-amplify'
-import awsExports from './aws-exports'
+import { awsconfig } from './aws-exports'  // <- use named import
 
-Amplify.configure(awsExports)
+Amplify.configure(awsconfig)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
