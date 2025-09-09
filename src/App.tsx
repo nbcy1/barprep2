8f28bb11683053ccbcfe37d67e0ae9a0ea95076b
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-09 17:49:08 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
-// import Quiz from './pages/Quiz'   // ❌ removed for now
 import History from './pages/History'
 import Account from './pages/Account'
 
@@ -13,7 +12,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/dashboard" element={<Dashboard />} />
-        {/* <Route path="/quiz" element={<Quiz />} />  ❌ removed */}
         <Route path="/history" element={<History />} />
         <Route path="/account" element={<Account />} />
       </Routes>
