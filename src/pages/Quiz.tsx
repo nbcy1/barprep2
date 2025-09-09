f5517139dfba9f7d3ac95c90a781e5ab84ec99fe
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-09 18:31:49 -0500

    Create Quiz.tsx

---
+++ src/pages/Quiz.tsx
@@ -0,0 +1,8 @@
+export default function Quiz() {
+  return (
+    <div className="page">
+      <h2>Quiz Page</h2>
+      <p>This feature is currently disabled for testing purposes.</p>
+    </div>
+  )
+}
