584ebe00ea655b79a877dbbf5b39997f7c0a38a2
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-09 18:26:07 -0500

    Update aws-exports.ts

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -1,1 +1,10 @@
+// src/aws-exports.ts
+
+export const awsconfig = {
+  aws_project_region: 'YOUR_REGION',
+  aws_cognito_region: 'YOUR_COGNITO_REGION',
+  aws_user_pools_id: 'YOUR_USER_POOL_ID',
+  aws_user_pools_web_client_id: 'YOUR_WEB_CLIENT_ID',
+  // add other Amplify config fields here
+}
 
