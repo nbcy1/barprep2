3bff288aeeae5280a97548558d2d54d848846629
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-09 18:57:28 -0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,14 @@
-import { Link } from "react-router-dom";
-// or from "next/link" if using Next.js
+// src/components/Navbar.tsx
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { useAuthenticator } from '@aws-amplify/ui-react'
 
 export default function Navbar() {
+  const { user, signOut } = useAuthenticator((context) => [context.user])
+
   return (
-    <nav className="navbar">
-      <h2>BarPrep</h2>
-      <div>
-        <Link to="/dashboard">Dashboard</Link> |{" "}
-        <Link to="/quiz">Quiz</Link> |{" "}
-        <Link to="/history">History</Link> |{" "}
-        <Link to="/account">Account</Link>
-      </div>
-    </nav>
-  );
-}
+    <nav style={{ padding: '1rem', borderBottom: '1px solid #ddd' }}>
+      <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
+      <Link to="/dashboard" style={{ marginRight: '1rem' }}>Dashboard</Link>
+      <Link to="/history" style={{ marginRight: '1rem' }}>History</Link>
+      <Link to="/account" style={{ marginRight: '1rem' }}>Account</Link>
