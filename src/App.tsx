595522132cbae6ccb7d1318bdeee264bc1f56510
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-09 18:47:24 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import Home from './pages/Home'
 import Dashboard from './pages/Dashboard'
 import History from './pages/History'
 import Account from './pages/Account'
+import Login from './pages/Login'
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/history" element={<History />} />
         <Route path="/account" element={<Account />} />
