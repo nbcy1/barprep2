403ec5260bd022ae799c801cc599ac7d833620e6
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-09 19:37:16 -0500

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,14 @@
-// src/components/Navbar.tsx
-import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default function Navbar() {
   return (
-    <nav style={{ padding: '1rem', borderBottom: '1px solid #ddd' }}>
+    <nav style={{ padding: '1rem', borderBottom: '1px solid #ccc' }}>
       <Link to="/" style={{ marginRight: '1rem' }}>Home</Link>
       <Link to="/dashboard" style={{ marginRight: '1rem' }}>Dashboard</Link>
+      <Link to="/questions" style={{ marginRight: '1rem' }}>Questions</Link>
       <Link to="/history" style={{ marginRight: '1rem' }}>History</Link>
       <Link to="/account" style={{ marginRight: '1rem' }}>Account</Link>
-      <Link to="/questions" style={{ marginRight: '1rem' }}>Questions</Link>
-      <Link to="/login" style={{ marginRight: '1rem' }}>Login</Link>
+      <Link to="/login">Login</Link>
     </nav>
   )
 }
