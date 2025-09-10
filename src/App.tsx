43a7956371b4ac7dc0775b26b715e5f75b59c00d
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-09 19:07:17 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+// src/App.tsx
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
@@ -5,6 +6,7 @@ import Dashboard from './pages/Dashboard'
 import History from './pages/History'
 import Account from './pages/Account'
 import Login from './pages/Login'
+import Questions from './pages/Questions'
 
 function App() {
   return (
@@ -12,10 +14,11 @@ function App() {
       <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/dashboard" element={<Dashboard />} />
         <Route path="/history" element={<History />} />
         <Route path="/account" element={<Account />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/questions" element={<Questions />} />
       </Routes>
     </Router>
   )
