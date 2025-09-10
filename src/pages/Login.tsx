1f3207688cd0e987a0cf56ff1b018ddaceddc5b5
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-09 19:33:33 -0500

    Update Login.tsx

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,19 +1,11 @@
-// src/pages/Login.tsx
 import React from 'react'
 import { Authenticator } from '@aws-amplify/ui-react'
-import '@aws-amplify/ui-react/styles.css'
 
 export default function Login() {
   return (
-    <div style={{ padding: '2rem' }}>
-      <Authenticator>
-        {({ signOut, user }) => (
-          <div>
-            <h2>Welcome, {user?.username}</h2>
-            <button onClick={signOut}>Sign Out</button>
-          </div>
-        )}
-      </Authenticator>
+    <div className="page">
+      <h2>Login</h2>
+      <Authenticator />
     </div>
   )
 }
