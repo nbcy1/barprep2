faa0d796c36883a0451990d000c63630f53ea34d
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-10 20:01:12 -0500

    Update seedQuestions.ts

--- seedQuestions.ts
+++ seedQuestions.ts
@@ -1,6 +1,13 @@
+import { Amplify } from "aws-amplify";
 import { generateClient } from "aws-amplify/data";
 import type { Schema } from "./amplify/data/resource";
 import fs from "fs";
+
+// Import your Gen 2 backend outputs
+import amplifyConfig from "./amplify/amplify_outputs.json";
+
+// Tell Amplify about your backend
+Amplify.configure(amplifyConfig);
 
 const questions = JSON.parse(fs.readFileSync("./questions.json", "utf-8"));
 
@@ -9,7 +16,7 @@ async function seed() {
 
   for (const q of questions) {
     try {
-      await client.models.Question.create(q);
+      await client.models.Todo.create(q); // <-- use your model name
       console.log(`Added question: ${q.content}`);
     } catch (err) {
       console.error("Error adding question:", q.content, err);
