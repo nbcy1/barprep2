ebf2f9af84bc21fff99e28d599a396827afc8cb7
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-10 19:04:25 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -5,9 +5,10 @@ import './index.css'
 
 // Amplify setup
 import { Amplify } from 'aws-amplify'
-import { awsconfig } from './aws-exports'  // <- use named import
+// Import the Gen 2 JSON outputs directly
+import amplifyConfig from '../amplify/amplify_outputs.json'
 
-Amplify.configure(awsconfig)
+Amplify.configure(amplifyConfig)
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
