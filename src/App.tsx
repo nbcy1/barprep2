86daa893e7b4cadbbcddb9a33db4babd804196f3
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-21 14:17:21 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Navbar from './components/Navbar'
-import Home from './pages/Home'
-import Dashboard from './pages/Dashboard'
-import History from './pages/History'
-import Account from './pages/Account'
-import Questions from './pages/Questions'
-import Login from './pages/Login'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Home from './pages/Home';
+import Dashboard from './pages/Dashboard';
+import History from './pages/History';
+import Account from './pages/Account';
+import Questions from './pages/Questions';
+import AdminQuestions from './pages/AdminQuestions';
+import Login from './pages/Login';
 
 function App() {
   return (
@@ -17,11 +18,11 @@ function App() {
         <Route path="/history" element={<History />} />
         <Route path="/account" element={<Account />} />
         <Route path="/questions" element={<Questions />} />
-        <Route path="/login" element={<Login />} />
         <Route path="/admin-questions" element={<AdminQuestions />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
-  )
+  );
 }
 
-export default App
+export default App;
