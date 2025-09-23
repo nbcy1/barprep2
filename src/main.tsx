36c5277c30755fe5570b4a1416353f2153a1689f
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-23 17:49:01 -0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,22 +1,13 @@
-// src/main.tsx
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
 
-// Amplify setup
 import { Amplify } from 'aws-amplify';
 import awsExports from './aws-exports';
-import { DataStore } from '@aws-amplify/datastore';
-import { API } from '@aws-amplify/api';
-import { Auth } from '@aws-amplify/auth';
 
-Amplify.configure({
-  ...awsExports,
-  API,
-  Auth,
-  DataStore,
-});
+// Configure Amplify with your project’s settings
+Amplify.configure(awsExports);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
