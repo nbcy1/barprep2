b33851b32a7a27959c3995f493c3ce981a26eb85
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-22 19:15:51 -0500

    Update aws-exports.ts

--- src/aws-exports.ts
+++ src/aws-exports.ts
@@ -1,3 +1,10 @@
-import amplifyConfig from './amplify_outputs.json'
+// src/aws-exports.ts
+const amplifyConfig = {
+  aws_project_region: "us-east-2",
+  aws_appsync_graphqlEndpoint: "https://4giplmig6zghxjg35olc5awjt4.appsync-api.us-east-2.amazonaws.com/graphql",
+  aws_appsync_region: "us-east-2",
+  aws_appsync_authenticationType: "API_KEY",
+  aws_appsync_apiKey: "da2-mvdc7tyw2jfhrk63h6f24pb3be"
+};
 
-export default amplifyConfig
+export default amplifyConfig;
