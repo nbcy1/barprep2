9994c85d9d7d1cddb883602dcb2b79c687eed3a5
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-26 18:44:51 -0500

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,6 +1,6 @@
 import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
-import { data } from './data/resource';  // keep if you have a data resource; else remove
+import { data } from './data/resource';
 
 defineBackend({
   auth,
