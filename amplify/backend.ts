aa8f3091da9bbfd6330b4b6f035cd47a3254e1f6
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-26 18:52:25 -0500

    Update backend.ts

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -2,6 +2,9 @@ import { defineBackend } from '@aws-amplify/backend';
 import { auth } from './auth/resource';
 import { data } from './data/resource';
 
+/**
+ * Define backend with ConstructFactory objects
+ */
 defineBackend({
   auth,
   data,
