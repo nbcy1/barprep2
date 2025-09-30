37152c998d45f6ebc10e4b83a27049ff061f72de
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-30 00:44:53 +0000

    Remove auth resource and update project files

---
+++ src/ui-components/QuestionCreateForm.d.ts
@@ -0,0 +1,60 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+import * as React from "react";
+import { GridProps, TextFieldProps } from "@aws-amplify/ui-react";
+export declare type EscapeHatchProps = {
+    [elementHierarchy: string]: Record<string, unknown>;
+} | null;
+export declare type VariantValues = {
+    [key: string]: string;
+};
+export declare type Variant = {
+    variantValues: VariantValues;
+    overrides: EscapeHatchProps;
+};
+export declare type ValidationResponse = {
+    hasError: boolean;
+    errorMessage?: string;
+};
+export declare type ValidationFunction<T> = (value: T, validationResponse: ValidationResponse) => ValidationResponse | Promise<ValidationResponse>;
+export declare type QuestionCreateFormInputValues = {
+    question?: string;
+    choices?: string[];
+    answer?: string;
+    topic?: string;
+    createdAt?: string;
+    updatedAt?: string;
+};
+export declare type QuestionCreateFormValidationValues = {
+    question?: ValidationFunction<string>;
+    choices?: ValidationFunction<string>;
+    answer?: ValidationFunction<string>;
+    topic?: ValidationFunction<string>;
+    createdAt?: ValidationFunction<string>;
+    updatedAt?: ValidationFunction<string>;
+};
+export declare type PrimitiveOverrideProps<T> = Partial<T> & React.DOMAttributes<HTMLDivElement>;
+export declare type QuestionCreateFormOverridesProps = {
+    QuestionCreateFormGrid?: PrimitiveOverrideProps<GridProps>;
+    question?: PrimitiveOverrideProps<TextFieldProps>;
+    choices?: PrimitiveOverrideProps<TextFieldProps>;
+    answer?: PrimitiveOverrideProps<TextFieldProps>;
+    topic?: PrimitiveOverrideProps<TextFieldProps>;
+    createdAt?: PrimitiveOverrideProps<TextFieldProps>;
+    updatedAt?: PrimitiveOverrideProps<TextFieldProps>;
+} & EscapeHatchProps;
+export declare type QuestionCreateFormProps = React.PropsWithChildren<{
+    overrides?: QuestionCreateFormOverridesProps | undefined | null;
+} & {
+    clearOnSuccess?: boolean;
+    onSubmit?: (fields: QuestionCreateFormInputValues) => QuestionCreateFormInputValues;
+    onSuccess?: (fields: QuestionCreateFormInputValues) => void;
+    onError?: (fields: QuestionCreateFormInputValues, errorMessage: string) => void;
+    onChange?: (fields: QuestionCreateFormInputValues) => QuestionCreateFormInputValues;
+    onValidate?: QuestionCreateFormValidationValues;
+} & React.CSSProperties>;
+export default function QuestionCreateForm(props: QuestionCreateFormProps): React.ReactElement;
