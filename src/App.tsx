918d249c14379893ffee7e1eeaf567641f964507
nbcy1 <nicolas.bc.yost@gmail.com>
2025-09-30 20:33:23 -0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { Authenticator } from '@aws-amplify/ui-react';
+import '@aws-amplify/ui-react/styles.css';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import Dashboard from './pages/Dashboard';
@@ -11,19 +13,21 @@ import Login from './pages/Login';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/history" element={<History />} />
-        <Route path="/account" element={<Account />} />
-        <Route path="/questions" element={<Questions />} />
-        <Route path="/quiz" element={<Quiz />} />
-        <Route path="/admin-questions" element={<AdminQuestions />} />
-        <Route path="/login" element={<Login />} />
-      </Routes>
-    </Router>
+    <Authenticator.Provider>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/history" element={<History />} />
+          <Route path="/account" element={<Account />} />
+          <Route path="/questions" element={<Questions />} />
+          <Route path="/quiz" element={<Quiz />} />
+          <Route path="/admin-questions" element={<AdminQuestions />} />
+          <Route path="/login" element={<Login />} />
+        </Routes>
+      </Router>
+    </Authenticator.Provider>
   );
 }
 
